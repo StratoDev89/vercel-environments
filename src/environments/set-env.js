333d260c40af9ac9d96b86ac86a03efd4c6662b8
StratoDev89 <danieltestingemail01@gmail.com>
2024-05-17 08:30:22 -0400

    test 2

--- src/environments/set-env.js
+++ src/environments/set-env.js
@@ -2,13 +2,12 @@ function setEnv() {
   fs = require("fs");
   writeFile = fs.writeFile;
 
-  targetPath = "/vercel/path1/src/environments/environment.prod.json";
-  targetPath2 = "/vercel/path2/src/environments/environment.prod.json";
+  targetPath = "./src/environments/environment.prod.json";
 
   envConfigFile = process.env.FIREBASE;
 
   console.log(
-    "The file `environment.prod.json` will be written with the following content: \n"
+    `The file `environment.prod.json` will be written with the following content: ${envConfigFile} \n`
   );
 
   writeFile(targetPath, envConfigFile, function (err) {
@@ -23,18 +22,7 @@ function setEnv() {
       );
     }
   });
-  writeFile(targetPath2, envConfigFile, function (err) {
-    if (err) {
-      console.error(err);
-      throw err;
-    } else {
-      console.log(
-        "Angular environment.prod.json file generated correctly at" +
-          targetPath +
-          "\n"
-      );
-    }
-  });
+
 }
 
 setEnv();
